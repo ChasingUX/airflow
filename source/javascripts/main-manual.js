cd47ee3352a65db0c34143138280499a83e417ef
ChasingUX <JBIRD1111@gmail.com>
2016-05-30 19:04:15 -0700

    major initial commit

---
+++ source/javascripts/main-manual.js
@@ -0,0 +1,356 @@
+jsPlumb.ready(function () {
+  // var dynamicAnchors = [ [ 0.5, 1, 0, 1 ], [ 0.5, 0, 0, -1 ],  [ 1, 0.5, 1, 0 ], [ 0, 0.5, -1, 0 ] ];
+
+  $('.window').each(function(){
+    $(this).append("<span class='add'>+</span>");
+
+    if($(this).data('sidebar')=='yes') {
+      $(this).append("<span class='hasSide'></span>")
+    }
+
+    var leftPos = $(this).css('left');
+    $(this).css('left', leftPos);
+
+  });
+
+  $('.wrapper').on("mouseenter", ".window", function() {
+      $(this).addClass('hover');
+  });
+
+  $('.wrapper').on("mouseleave", ".window", function() {
+      $(this).removeClass('hover');
+  });
+
+  var instance = window.jsp = jsPlumb.getInstance({
+      DragOptions: { cursor: 'pointer', zIndex: 2000 },
+      ConnectionOverlays: [
+        [ "Arrow", {
+          location: 1,
+          visible:true,
+          width:9,
+          length:9,
+          foldback:1,
+          id:"ARROW",
+          paintStyle: {strokeStyle: "#f5f5f5", lineWidth: 3 }
+        } ],
+        [ "Label", {
+          location: 0.73,
+          id: "label",
+          cssClass: "aLabel",
+          paintStyle: {fillStyle: "#00d1c1" }
+        }]
+    ],
+    Container: "canvas"
+  });
+
+  var basicType = {
+    connector: "StateMachine",
+    paintStyle: { strokeStyle: "#ff5a5f", fillStyle: "white", radius: 5, lineWidth: 1.4 },
+    isSource: true,
+    connectorStyle: connectorPaintStyle,
+    hoverPaintStyle: endpointHoverStyle,
+    connectorHoverStyle: connectorHoverStyle,
+    overlays: [
+      "Arrow"
+    ]
+  };
+
+  instance.registerConnectionType("basic", basicType);
+
+  // this is the paint style for the connecting lines..
+  var connectorPaintStyle = {
+    lineWidth: 1,
+    strokeStyle: "#00d1c1",
+    joinstyle: "round",
+    outlineColor: "#F5F5F5",
+    outlineWidth: 1
+  },
+// .. and this is the hover style.
+  connectorHoverStyle = {
+    strokeStyle: "#ff5a5f",
+  },
+  endpointHoverStyle = {
+    fillStyle: "#ff5a5f",
+    strokeStyle: "#ff5a5f"
+  },
+  targetHoverStyle = {
+    fillStyle: "transparent"
+  },
+// the definition of source endpoints 
+  sourceEndpoint = {
+    endpoint: "Dot",
+    paintStyle: { strokeStyle: "#ff5a5f", fillStyle: "white", radius: 4, lineWidth: 1.2 },
+    isSource: true,
+    maxConnections: 4,
+    connector: [ "Flowchart", { stub: [40, 60], gap: 10, cornerRadius: 5, alwaysRespectStubs: true } ],
+    connectorStyle: connectorPaintStyle,
+    hoverPaintStyle: endpointHoverStyle,
+    connectorHoverStyle: connectorHoverStyle,
+    dragOptions: {},
+      
+  },
+// the definition of target endpoints (will appear when the user drags a connection)
+  targetEndpoint = {
+    endpoint: "Dot",
+    
+    cssClass: "dropZone",
+    paintStyle: { fillStyle: "transparent", radius: 5},
+    hoverPaintStyle: targetHoverStyle,
+    maxConnections: 4,
+    dropOptions: { hoverClass: "hover", activeClass: "active" },
+    isTarget: true,
+    overlays: [
+      [ "Label", { location: [0.5, -0.5], label: "Drop", cssClass: "endpointTargetLabel", visible:false } ]
+    ]
+  },
+  init = function (connection) {
+    connection.getOverlay("label").setLabel(connection.sourceId.substring(15) + "-" + connection.targetId.substring(15));
+  };
+
+  var _addEndpoints = function (toId, sourceAnchors, targetAnchors) {
+    for (var i = 0; i < sourceAnchors.length; i++) {
+      var sourceUUID = toId + sourceAnchors[i];
+      instance.addEndpoint("flowchart" + toId, sourceEndpoint, {
+        //anchor: dynamicAnchors, uuid: sourceUUID
+        anchor: sourceAnchors[i], uuid: sourceUUID
+      });
+    }
+    for (var j = 0; j < targetAnchors.length; j++) {
+      var targetUUID = toId + targetAnchors[j];
+      instance.addEndpoint("flowchart" + toId, targetEndpoint, { anchor: targetAnchors[j], uuid: targetUUID });
+    }
+  };
+
+  // suspend drawing and initialise.
+  instance.batch(function () {
+    _addEndpoints("Window4", ["BottomCenter"], ["TopCenter", "LeftMiddle", "RightMiddle"]);
+    _addEndpoints("Window2", ["LeftMiddle", "BottomCenter", "RightMiddle"], []);
+    _addEndpoints("Window3", ["BottomCenter"], ["TopCenter", "LeftMiddle", "RightMiddle"]);
+    _addEndpoints("Window1", ["BottomCenter"], ["TopCenter", "LeftMiddle", "RightMiddle"]);
+
+    var child = jsPlumb.getSelector(".flowchart-demo .window");
+
+    // listen for new connections; initialise them the same way we initialise the connections at startup.
+    instance.bind("connection", function (connInfo, originalEvent) {
+        init(connInfo.connection);
+    });
+   
+    instance.draggable(child, {
+      containment: ".wrapper",
+      grid: [20, 20],
+      start: function(event, ui) {
+        $("#" + event.el.id + "").addClass('noclick');
+      }
+    });
+
+    $('.wrapper').on('click', '.window', function(event){
+      var $this = $(this),
+      add = $this.find('.add'),
+      target = event.target;
+
+      if(target == add[0]) {
+        //console.log('add a new thing and connect to this box');
+        AddDiv($this);
+
+      } else {
+        if ($this.hasClass('noclick')) {
+          $this.removeClass('noclick');
+        }
+        else {
+          // if(!$('body').hasClass('panel')){
+            
+          // }
+
+          if($this.data('sidebar')=='yes') {
+            //this is our card click event which ignores the drag event
+            
+            $('.window').removeClass('panelShowing');
+            openPanel($this);
+            $this.addClass('panelShowing');
+
+          }
+        }
+      }
+    })
+
+    //jsPlumb.revalidate("container");
+
+    // connect a few up
+    instance.connect({uuids: ["Window2BottomCenter", "Window4TopCenter"], editable: true});
+    instance.connect({uuids: ["Window2RightMiddle", "Window3TopCenter"], editable: true});
+    instance.connect({uuids: ["Window2LeftMiddle", "Window1TopCenter"], editable: true});
+      
+
+    //
+    // listen for clicks on connections, and offer to delete connections on click.
+    //
+    // instance.bind("click", function (conn, originalEvent) {
+    //    // if (confirm("Delete connection from " + conn.sourceId + " to " + conn.targetId + "?"))
+    //      //   instance.detach(conn);
+    //     //conn.toggleType("basic");
+    //     alert('sdf')
+    // });
+
+    // instance.bind("connectionDrag", function (connection) {
+    //     console.log("connection " + connection.id + " is being dragged. suspendedElement is ", connection.suspendedElement, " of type ", connection.suspendedElementType);
+    // });
+
+    // instance.bind("connectionDragStop", function (connection) {
+    //     console.log("connection " + connection.id + " was dragged");
+    // });
+
+    // instance.bind("connectionMoved", function (params) {
+    //     console.log("connection " + params.connection.id + " was moved");
+    // });
+  });
+
+   // jsPlumb.fire("jsPlumbDemoLoaded", instance);
+
+  // ZOOMING
+  jsPlumb.setContainer("Wrapper");
+  window.setZoom = function(zoom, instance, transformOrigin, el) {
+    transformOrigin = transformOrigin || [ 0.5, 0.5 ];
+    instance = instance || jsPlumb;
+    el = el || instance.getContainer();
+    var p = [ "webkit", "moz", "ms", "o" ],
+        s = "scale(" + zoom + ")",
+        oString = (transformOrigin[0] * 100) + "% " + (transformOrigin[1] * 100) + "%";
+
+    for (var i = 0; i < p.length; i++) {
+      el.style[p[i] + "Transform"] = s;
+      el.style[p[i] + "TransformOrigin"] = oString;
+    }
+
+    el.style["transform"] = s;
+    el.style["transformOrigin"] = oString;
+
+    instance.setZoom(zoom);    
+  };
+
+  var currentZoom = instance.getZoom();
+  
+  $('.zoomIn').on('click', function(){
+    currentZoom = currentZoom * 1.1;
+    window.setZoom(currentZoom);
+  });
+
+  $('.zoomOut').on('click', function(){
+    currentZoom = currentZoom / 1.1;
+    window.setZoom(currentZoom);
+  });
+
+  ///// END ZOOM ///////
+
+  var count = 4;
+
+  function AddDiv(clicked) {
+    count = count + 1;
+
+    var left = clicked.css('left');
+
+    var previosTopOffset = clicked.offset().top + clicked.outerHeight() + 40,
+     // parentLeftOffset = clicked.parent().offset().left,
+     // previosLeftOffset = clicked.offset().left,
+     // leftOffset = previosLeftOffset - parentLeftOffset,
+     Div = $('<div>', { id: "flowchartWindow" + count + "", class:"dyno" }).appendTo('#canvas').css({top: previosTopOffset, left: left});
+
+    //instance.addEndpoint("flowchartWindow" + count + "", sourceEndpoint, { anchor: dynamicAnchors});
+
+    instance.addEndpoint("flowchartWindow" + count + "", targetEndpoint, { anchor: "TopCenter"});
+    instance.addEndpoint("flowchartWindow" + count + "", targetEndpoint, { anchor: "LeftMiddle"});
+    instance.addEndpoint("flowchartWindow" + count + "", targetEndpoint, { anchor: "RightMiddle"});
+    instance.addEndpoint("flowchartWindow" + count + "", sourceEndpoint, { anchor: "BottomCenter"});
+
+    instance.draggable($(Div), {
+      containment: ".wrapper",
+      grid: [20, 20],
+      start: function(event, ui) {
+        $("#" + event.el.id + "").addClass('noclick');
+      }
+    });
+
+    //instance.connect({uuids: ["Window2LeftMiddle", "Window1TopCenter"], editable: true});
+
+    instance.connect({
+      source:clicked.attr('id'), 
+      target:"flowchartWindow" + count + "",
+      //endpointStyle:targetEndpoint,
+      endpointStyle:{radius:1, fillStyle: "transparent"},
+      paintStyle: { lineWidth: 1, strokeStyle: "#00d1c1", joinstyle: "round", outlineColor: "#F5F5F5", outlineWidth: 1 },
+      hoverPaintStyle: endpointHoverStyle,
+      anchors:["Bottom", "Top"],
+      endpoint:"Dot",
+      connector: [ "Flowchart", { stub: [40, 60], gap: 10, cornerRadius: 5, alwaysRespectStubs: true } ]
+    });
+
+    $(Div).addClass('window');
+
+    instance.repaintEverything();
+
+    $(Div).append('<span class="add">+</span><form action="#" class="nameMe" id="form"><input type="text" placeholder="Enter Label"><input type="submit"></form>');
+
+    $(".nameMe input[type='text']").focus();
+
+    $('.nameMe').keypress( function( e ) {
+      var code = e.keyCode || e.which;
+
+      if( code === 13 ) {
+        e.preventDefault();
+        var newValue = $(this).find('input').val();
+        //$(Div).find('.nameMe').remove().append('<p>' + $(this).find('input').val() + '</p>');
+
+        
+        $(Div).find('.nameMe').remove();
+        $(Div).append('<p>' + newValue + '</p>');
+
+        return false; 
+      }
+    }) 
+    // init = function (connection) {
+    //     connection.getOverlay("label").setLabel();
+    // };
+  } 
+
+  $( window ).resize(function() {
+    instance.repaintEverything();
+  });
+
+  function repaint(){
+    instance.repaintEverything();
+  }
+
+  instance.repaintEverything();
+
+  function openPanel(toLoad){
+    $('body').addClass('panel');
+    $('aside').find('*').not('.close').remove();
+    var refreshInterval = setInterval(repaint, 20);
+
+    setTimeout(function(){
+      clearInterval(refreshInterval);
+    }, 500);
+
+    var image = $(toLoad).data('image'),
+    description = $(toLoad).data('description'),
+    copy = $(toLoad).text().slice(0, -1);
+
+    $('aside').append("<h3>Flow Step:<span>" + copy + "</span></h3><img src='../images/" + image + "'><p>"+ description +"</p>")
+  }
+
+  function closePanel(){
+    $('body').removeClass('panel');
+    $('.window').removeClass('panelShowing');
+
+    var refreshInterval = setInterval(repaint, 20);
+
+    setTimeout(function(){
+      clearInterval(refreshInterval);
+    }, 500) 
+  }
+
+  var isDragging = false;
+
+  $('.close').on('click', function(){
+    closePanel();
+  });
+});
---
+++ source/javascripts/mainB.js
@@ -0,0 +1,260 @@
+jsPlumb.ready(function () {
+  var numWindows = 0;
+
+  $('.window').each(function(){
+    numWindows = numWindows + 1;
+
+    if($(this).data('sidebar')=='yes') {
+      $(this).append("<span class='hasSide'></span><nav class='flowActions'><span class='add'>new</span><span class='image'>img</span></nav>");
+    } else {
+      $(this).append("<nav class='flowActions'><span class='add'>add</span></nav>");
+    }
+
+    var leftPos = $(this).css('left');
+    $(this).css('left', leftPos);
+
+  });
+
+  $('.wrapper').on("mouseenter", ".window", function() {
+      $(this).addClass('hover');
+  });
+
+  $('.wrapper').on("mouseleave", ".window", function() {
+      $(this).removeClass('hover');
+  });
+
+
+  var connectorPaintStyle = {
+    lineWidth: 1,
+    strokeStyle: "#00d1c1",
+    joinstyle: "round",
+    outlineColor: "#F5F5F5",
+    outlineWidth: 1
+  },
+  connectorHoverStyle = {
+    strokeStyle: "#ff5a5f",
+  },
+  endpointHoverStyle = {
+    fillStyle: "#ff5a5f",
+    strokeStyle: "#ff5a5f"
+  },
+  targetHoverStyle = {
+    fillStyle: "transparent"
+  },
+  sourceStyle = {
+    strokeStyle: "#ff5a5f",
+    fillStyle: "white",
+    radius: 4,
+    lineWidth: 1.2 
+  },
+  targetStyle = {
+    strokeStyle: "transparent",
+    fillStyle: "transparent",
+    radius: 4,
+    lineWidth: 1.2 
+  },
+  instance = window.instance = jsPlumb.getInstance({
+    DragOptions: { cursor: 'pointer', zIndex: 2000 },
+    PaintStyle: connectorPaintStyle,
+    ConnectionOverlays: [
+      [ "Arrow", {
+        location: 1,
+        visible:true,
+        width:9,
+        length:9,
+        foldback:1,
+        id:"ARROW",
+        paintStyle: {strokeStyle: "#f5f5f5", lineWidth: 3 }
+      } ]
+    ],
+    Container: "canvas"
+  }),
+  draggableSettings = {
+    containment: ".wrapper",
+    grid: [20, 20],
+    start: function(event, ui) {
+      $("#" + event.el.id + "").addClass('noclick');
+    }
+  },
+  makeSourceSettings = {
+    filter:"span",
+    filterExclude:true,
+    anchor:[ "Perimeter", { shape:"Rectangle", anchorCount:16 }],
+    endpoint: "Dot",
+    paintStyle: sourceStyle,
+    isSource: true,
+    maxConnections: -1,
+    connector: [ "Flowchart", 
+      { 
+        stub: 11, 
+        gap: 10, 
+        cornerRadius: 7, 
+        alwaysRespectStubs: false 
+      } 
+    ],
+    hoverPaintStyle: endpointHoverStyle,
+    connectorHoverStyle: connectorHoverStyle
+  }
+  makeTargetSettings = {
+    dropOptions: { hoverClass: "hover" },
+    anchor:[ "Perimeter", { shape:"Rectangle", anchorCount:16 }],
+    paintStyle: targetStyle,
+    endpoint: "Dot",
+    maxConnections: -1
+  }
+
+  function repaint(){
+    instance.repaintEverything();
+  }
+
+  var allWindows = jsPlumb.getSelector(".window");   
+
+  instance.draggable(allWindows, draggableSettings);
+  instance.toggleDraggable(allWindows);
+
+  function enableDrag() {
+    allWindows = jsPlumb.getSelector(".window");   
+    instance.setSourceEnabled(allWindows, false);
+    instance.toggleDraggable(allWindows);
+  }
+
+  function disableDrag (){
+    allWindows = jsPlumb.getSelector(".window");   
+    instance.setSourceEnabled(allWindows, true);
+    instance.toggleDraggable(allWindows);
+  }
+
+  instance.batch(function () {
+    instance.makeSource(allWindows, makeSourceSettings);
+    instance.makeTarget(allWindows, makeTargetSettings);
+
+    // connect a few up
+    instance.connect({ source:'flowchartWindow2', target:'flowchartWindow4' });
+    instance.connect({ source:'flowchartWindow2', target:'flowchartWindow3' });
+    instance.connect({ source:'flowchartWindow2', target:'flowchartWindow1' });
+
+
+    //add div id we click on add
+    $('.wrapper').on('click', '.window', function(event){
+      var $this = $(this),
+      add = $this.find('.add'),
+      target = event.target;
+
+      if(target == add[0]) {
+        AddDiv($this);
+      } else {
+        if($this.data('sidebar')=='yes' && $('body').hasClass('flow')) {
+          //this is our card click event which ignores the drag event
+          
+          $('.window').removeClass('panelShowing');
+          openPanel($this);
+          $this.addClass('panelShowing');
+        }
+      }
+    });
+
+    $( window ).resize(function() {
+      repaint();
+    });
+  });
+
+  //switching between move and flow modes
+  $(".tools a").on('click', function(){
+    var thisType = $(this).attr('id');
+    $('body').removeClass('move flow').addClass(thisType);
+
+    $(".dragToggle").toggleClass('on');
+
+    if($('#move').hasClass('on')) {
+      enableDrag();
+    } else {
+      disableDrag();
+    }
+  })
+
+  ///// START ADD NEW BOX ///////
+
+  function AddDiv(clicked) {
+    numWindows = numWindows + 1;
+
+    var left = clicked.css('left'),
+      previosTopOffset = clicked.offset().top + clicked.outerHeight() + 40,
+      Div = $('<div>', { id: "flowchartWindow" + numWindows + "", class:"dyno" }).appendTo('#canvas').css({top: previosTopOffset, left: left});
+
+    instance.makeSource("flowchartWindow" + numWindows + "", makeSourceSettings);
+    instance.makeTarget("flowchartWindow" + numWindows + "", makeTargetSettings);
+
+    instance.draggable($(Div), draggableSettings);
+    instance.toggleDraggable($(Div));
+    
+    instance.connect({ source: clicked.attr('id'), target:"flowchartWindow" + numWindows + "" });
+
+    $(Div).addClass('window');
+
+    repaint();
+
+    // for now a sidebar data attr is not added, but woudl liek to build this in when I offer image upload, so leaving it here
+    if($(this).data('sidebar')=='yes') {
+      $(Div).append("<span class='hasSide'></span><nav class='flowActions'><span class='add'>new</span><span class='image'>img</span></nav><form action='#'' class='nameMe' id='form'><input type='text' placeholder='Enter Label'><input type='submit'></form>");
+    } else {
+      $(Div).append("<nav class='flowActions'><span class='add'>add</span></nav><form action='#'' class='nameMe' id='form'><input type='text' placeholder='Enter Label'><input type='submit'></form>");
+    }
+
+    $(".nameMe input[type='text']").focus();
+
+    $('.nameMe').keypress( function( e ) {
+      var code = e.keyCode || e.which;
+
+      if( code === 13 ) {
+        e.preventDefault();
+        var newValue = $(this).find('input').val();
+        //$(Div).find('.nameMe').remove().append('<p>' + $(this).find('input').val() + '</p>');
+
+        
+        $(Div).find('.nameMe').remove();
+        $(Div).append('<p>' + newValue + '</p>');
+
+        return false; 
+      }
+    }) 
+  } 
+
+  ///// END ADD NEW BOX ///////
+
+
+  ///// START PANEL OPEN & CLOSE ///////
+  function openPanel(toLoad){
+    $('body').addClass('panel');
+    $('aside').find('*').not('.close').remove();
+    var refreshInterval = setInterval(repaint, 20);
+
+    setTimeout(function(){
+      clearInterval(refreshInterval);
+    }, 500);
+
+    var image = $(toLoad).data('image'),
+    description = $(toLoad).data('description'),
+    copy = $(toLoad).find('p').text();
+
+
+    $('aside').append("<h3>Flow Step:<span>" + copy + "</span></h3><img src='../images/" + image + "'><p>"+ description +"</p>")
+  }
+
+  function closePanel(){
+    $('body').removeClass('panel');
+    $('.window').removeClass('panelShowing');
+
+    var refreshInterval = setInterval(repaint, 20);
+
+    setTimeout(function(){
+      clearInterval(refreshInterval);
+    }, 500) 
+  }
+
+  ///// END PANEL OPEN & CLOSE ///////
+
+
+  $('.close').on('click', function(){
+    closePanel();
+  });
+}); 
